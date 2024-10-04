2594bb540e01bbe8f06e0b5fb13c711883ddc4a4
machariamarigi <machariamarigi@gmail.com>
2024-10-04 09:45:17 +0300

    add POST, GET, PUT & DELETE for '/orders' & 'orders/:id' endpoints

--- application/routes.go
+++ application/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/machariamarigi/orders-api/handler"
 )
 
 func loadRoutes() *chi.Mux {
@@ -15,5 +16,16 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	router.Route("/orders", loadOrderRoutes)
 	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
